01987f4fd41138d6acc88328b72ec07b5661a620
lijinglun <lijinglun@graphicchina.com>
2019-09-05 09:53:29 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z "$STAGE" ]]; then
+    echo STAGE invalid
+    exit 1
+fi
+
+if [[ -z "$AWS_ACCESS_KEY_ID" ]]; then
+    echo AWS Access Key ID invalid
+    exit 1
+fi
+
+if [[ -z "$AWS_SECRET_ACCESS_KEY" ]]; then
+    echo AWS Secret Access Key invalid
+    exit 1
+fi
+
+ls
+serverless config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
+serverless deploy -s $STAGE
