806b5a623a33369809da8b4160fefd92dfe961ad
lijinglun <lijinglun@graphicchina.com>
2019-09-05 16:20:33 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,19 @@ st=$(git branch)
 echo $st
 st=${st##*/}
 echo $st
+
+BRANCH=$(git branch)
+BRANCH=${BRANCH##*/}
+STAGE="_"
+pwd
+ls
+echo $BRANCH
+echo $STAGE
+if [[ "${BRANCH}"=="master" ]]; then
+    STAGE="production"
+elif [[ "${BRANCH}"=="develop" ]]; then
+    STAGE="dev"
+elif [[ "${BRANCH}"=="qa4" ]]; then
+    STAGE="qa4"
+fi
+echo using STAGE $STAGE
